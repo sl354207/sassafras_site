b11fc5770db67e8c48e33e13e3c4c614fa04cf1c
sl354207 <sl354207@ohio.edu>
2021-02-26 16:02:10 -0500

    removed netlify-cms

--- .eleventy.js
+++ .eleventy.js
@@ -44,8 +44,6 @@ module.exports = function (eleventyConfig) {
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
   
-  // Copy favicon to route of /_site
-  // eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
